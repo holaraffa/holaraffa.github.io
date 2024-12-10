4a1387a4d6538a1f6de2132b449026cc988ab0c2
AngeloCipriani <angelo13cipriani.03@gmail.com>
2024-12-10 19:24:22 +0100

    Deploy sito web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2512294238"
+    serviceWorkerVersion: "358871813"
   }
 });
